807bd5a85d3b59e147699cfcf181badba0af9807
rajkuku <rajkuku@RAJKUKU-LAP.in.oracle.com>
2018-05-03 13:01:56 +0530

    Added HelloJenkings java files

---
+++ src/com/test/HelloJenkinTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class HelloJenkinTest {
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello Jenkins..."+i);
+		}
+	}
+
+}
